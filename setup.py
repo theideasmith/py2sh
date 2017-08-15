a425f9b9bcd2e5113ca3d11bbfe3d39bada5cb32
Akiva Lipshitz <aclscientist@gmail.com>
2017-08-15 10:23:46 -0800

    fixed breaking bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'py2sh',
   packages = ['py2sh'], # this must be the same as the name above
-  version = '0.0.1',
+  version = 'v0.0.3',
   license='MIT',
   description = 'A python CLI for busy scientists',
   author = 'Akiva Lipshitz',
@@ -13,7 +13,7 @@ setup(
        ]
   },
   url = 'https://github.com/theideasmith/py2sh', # use the URL to the github repo
-  download_url = 'https://github.com/theideasmith/py2sh/archive/0.0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/theideasmith/py2sh/archive/0.0.3.tar.gz', # I'll explain this in a second
   keywords = ['cli', 'command', 'argparse', 'click', 'script', 'function','bash', 'py2sh'], # arbitrary keywords
   classifiers = [],
 )
